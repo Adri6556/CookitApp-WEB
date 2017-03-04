7f6eaffb1c12e6a4063d08a505c1b17dd77d84ba
Adri6556 <adriii6556@gmail.com>
2017-03-04 02:39:50 +0100

    gerhger
    herher

--- view/viewLogin.php
+++ view/viewLogin.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Material Design Bootstrap Template</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/css.css" rel="stylesheet">
+
+    <!-- Material Design Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+</head>
 <body>
 <div class="login-dark">
     <form method="post">
@@ -14,3 +34,4 @@
         </div><a href="#" class="forgot">Forgot your email or password?</a></form>
 </div>
 </body>
+</html>
